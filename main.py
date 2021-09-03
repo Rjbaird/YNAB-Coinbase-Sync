a17a2e92e770f48a08012eb05f0135d1f76a00ae
Rjbaird <rjbaird09@gmail.com>
2021-09-03 12:03:49 -0500

    remove bot functions and added main function to main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from datetime import date
 from dotenv import load_dotenv
 from ynab import get_ynab_account_balance, update_account_balance, get_account_change
 from coinbase import get_wallet_data, get_coinbase_account_balance
-from bot import send_message, create_update_message, start_discord_bot
 
 load_dotenv()
 
@@ -13,15 +12,16 @@ ACCOUNT_ID = os.getenv('ACCOUNT_ID')
 COIN_KEY = os.getenv('COINBASE_KEY')
 COIN_SECRET = os.getenv('COINBASE_SECRET')
 EXCHANGE_KEY = os.getenv('EXCHANGE_KEY')
-BOT_TOKEN = os.getenv('DISCORD_TOKEN')
 
 today = date.today()
 YNAB_transaction_date = today.strftime('%Y-%m-%d')
 
-ynab_balance = get_ynab_account_balance(YNAB_TOKEN, BUDGET_ID, ACCOUNT_ID)
-wallet_data = get_wallet_data(COIN_KEY, COIN_SECRET)
-portfolio_balance = get_coinbase_account_balance(EXCHANGE_KEY, wallet_data)
-account_change = get_account_change(portfolio_balance, ynab_balance)
-update_account_balance(YNAB_TOKEN, BUDGET_ID, ACCOUNT_ID, account_change, YNAB_transaction_date)
-bot_message = create_update_message(account_change, portfolio_balance)
+def main():
+    ynab_balance = get_ynab_account_balance(YNAB_TOKEN, BUDGET_ID, ACCOUNT_ID)
+    wallet_data = get_wallet_data(COIN_KEY, COIN_SECRET)
+    portfolio_balance = get_coinbase_account_balance(EXCHANGE_KEY, wallet_data)
+    account_change = get_account_change(portfolio_balance, ynab_balance)
+    update_account_balance(YNAB_TOKEN, BUDGET_ID, ACCOUNT_ID, account_change, YNAB_transaction_date)
 
+if __name__ == '__main__':
+    main()
